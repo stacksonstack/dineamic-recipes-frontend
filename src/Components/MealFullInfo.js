e5715d0a8418f2e4964268d0771aa49786c58ed9
stacksonstack <staceycarrillo44@gmail.com>
2020-12-02 17:26:58 -0500

    show basic grid layout finished, add more styling

--- src/Components/MealFullInfo.js
+++ src/Components/MealFullInfo.js
@@ -8,13 +8,15 @@ class MealFullInfo extends Component{
             <div>
                 
                 <h1>{this.props.meal.name}</h1>
-                <h2>Food Category: {this.props.meal.category}</h2>
-                <h2>Food Origin: {this.props.meal.origin}</h2>
-                <h2>YouTube Link: {this.props.meal.youtube_link}</h2>
-                <img alt={this.props.meal.name} src={this.props.meal.image}/>
-                <p>Instruction: {this.props.meal.instructions} </p>
-                <p>Ingredients: {this.props.meal.ingredient}</p>
-                <p>Measurements: {this.props.meal.measurement}</p>
+                <div id="show-container">
+                    <div id="show-category"><h2>Food Category: {this.props.meal.category}</h2></div>
+                    <div id="show-origin"><h2>Food Origin: {this.props.meal.origin}</h2></div>
+                    <div id="show-youtube"><h2>YouTube Link: {this.props.meal.youtube_link}</h2></div>
+                    <div id="show-image"><img alt={this.props.meal.name} src={this.props.meal.image}/></div>
+                    <div id="show-instructions"><p>Instruction: {this.props.meal.instructions} </p></div>
+                    <div id="show-ingredients"><p>Ingredients: {this.props.meal.ingredient}</p></div>
+                    <div id="show-measurements"><p>Measurements: {this.props.meal.measurement}</p></div>
+                </div>
             </div>
         )
     }
